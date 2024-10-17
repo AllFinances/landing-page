5fa8c2c9a4928512a3bb0657d1f3d1728b7fff68
reinis <reinis.gangis@ftm.lv>
2024-10-17 19:19:04 +0300

    UPDATE: tailwind nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,17 @@ export default defineNuxtConfig({
 	compatibilityDate: "2024-04-03",
 	devtools: { enabled: true },
 	css: ["~/assets/css/main.css"],
+
 	postcss: {
 		plugins: {
 			tailwindcss: {},
 			autoprefixer: {},
 		},
 	},
+
 	app: {
-		buildAssetsDir: "/landing-page/"
-	}
+		buildAssetsDir: "/landing-page/",
+	},
+
+	modules: ["@nuxtjs/tailwindcss"],
 });
